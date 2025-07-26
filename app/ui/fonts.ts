e2c4b6a02353b0a27e90c4016868eb19ceac398d
sourajit <sourajit@gmail.com>
2025-07-26 11:25:56 +0530

    change font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,11 @@
-import { Inter } from 'next/font/google';
+import { Inter, Lusitana } from 'next/font/google';
 
 export const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+
+export const lusitana = Lusitana({
   subsets: ['latin'],
   weight: ['400', '700'],
 });
